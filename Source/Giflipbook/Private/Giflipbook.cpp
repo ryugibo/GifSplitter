770203e756ac1d580cd91c0348048e75e4d7b02f
ryugibo <ryugibo@daum.net>
2020-08-31 12:32:01 +0900

    Create Empty Plugin
    ww

---
+++ Source/Giflipbook/Private/Giflipbook.cpp
@@ -0,0 +1,20 @@
+// Copyright 2018 Ryugibo, Inc. All Rights Reserved.
+
+#include "Giflipbook.h"
+
+#define LOCTEXT_NAMESPACE "FGiflipbookModule"
+
+void FGiflipbookModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FGiflipbookModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FGiflipbookModule, Giflipbook)
